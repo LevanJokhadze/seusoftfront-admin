2bff3134ac0fb8978c14065f71cabae4b6eff132
lawliet005 <sabalomsadze912@gmail.com>
2024-08-02 00:41:13 +0400

    update editor

--- src/Components/pages/dashboard/Dashboard.jsx
+++ src/Components/pages/dashboard/Dashboard.jsx
@@ -151,22 +151,24 @@ const ItemForm = ({ item, onSubmit, onCancel, isEditMode }) => {
       {!type ? (
         <div className="form-group">
           <Editor
-            apiKey={process.env.REACT_APP_TINYMCE_API_KEY}
+          apiKey={process.env.REACT_APP_TINYMCE_API_KEY}
             value={formData.body}
-            init={{
-              height: 300,
-              menubar: false,
-              plugins: [
-                'advlist autolink lists link image charmap print preview anchor',
-                'searchreplace visualblocks code fullscreen',
-                'insertdatetime media table paste code help wordcount'
-              ],
-              toolbar: 'undo redo | formatselect | bold italic backcolor | \
-                alignleft aligncenter alignright alignjustify | \
-                bullist numlist outdent indent | removeformat | help'
-            }}
-            onEditorChange={handleEditorChange}
-          />
+          init={{
+            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+            tinycomments_mode: 'embedded',
+            tinycomments_author: 'Author name',
+            mergetags_list: [
+              { value: 'First.Name', title: 'First Name' },
+              { value: 'Email', title: 'Email' },
+            ],
+            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+          }}
+          onEditorChange={handleEditorChange}
+          onInit={(evt, editor) => {
+            console.log('Editor is ready to use!', editor);
+          }}
+        />
         </div>
       ) : (
         <div className="multi-item-container">
